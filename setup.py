55fd38a22b003bb3304631d9145007378b4dfd18
therudevich <160277811+therudevich@users.noreply.github.com>
2024-02-17 00:24:32 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tonroll',
+    version='1.0.0',
+    description='A library for handling tonrolling',
+    packages=find_packages(),
+    author='Rude',
+    author_email='rude.rudevich@mail.ru',
+    url='https://github.com/therudevich/tonroll',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
